82e68418d52e8ed28c4e24487f855df560f32214
Jacob Homanics <homanicsjake@gmail.com>
2024-12-16 22:49:04 -0600

    up

--- Assets/Scripts/BtnInteractableToGoldCount.cs
+++ Assets/Scripts/BtnInteractableToGoldCount.cs
@@ -4,15 +4,21 @@ using UnityEngine.UI;
 public class BtnInteractableToGoldCount : MonoBehaviour
 {
 
-    public GoldCount goldCount;
+    // public GoldCount goldCount;
 
     public Button btn;
 
 
-    public PriceItem priceItem;
+    // public PriceItem priceItem;
+
+
+    public Number gold;
+    public NumberDependentValue price;
 
     void Update()
     {
+        btn.interactable = gold.Value >= price.Value;
+
         // btn.interactable = goldCount.gold.Value >= priceItem.price;
     }
 }
